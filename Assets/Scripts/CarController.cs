972e9a1887285a1dfe70258689d7e7d60f7f2990
David Varo <david.vama21@gmail.com>
2024-11-08 16:49:16 +0100

    Controles de coche con Wheel Collider (basado en tutorial Unity, incompleto)
    Se han hecho pruebas de código para controlar el coche, al que ahora se le añadido modelo encontrado en la Asset Store.

--- Assets/Scripts/CarController.cs
+++ Assets/Scripts/CarController.cs
@@ -5,23 +5,31 @@ using UnityEngine;
 [RequireComponent( typeof(Rigidbody) )]
 public class CarController : MonoBehaviour
 {
-    public float acceleration = 50; // velocidad a la que aceleramos si nos movemos
-    public float MaxSpeed = 15;
+    //public float acceleration = 50; // velocidad a la que aceleramos si nos movemos
+    public float motorTorque = 2000;
+    public float maxSpeed = 15;
+    public float turnAngle = 30; // parámetro que indica máximo giro
+    public float turnAngleAtMaxSpeed = 10;
+
+    public float centreOfGravityOffset = -1f;
 
 
     // --> private Vector3 currentSpeed; // (en dirección movimiento)
-    private float movement; // indicará si debemos movernos, para iniciar acción en FixedUpdate 
+    private float movement; // indicará si debemos movernos, para iniciar acción en FixedUpdate
+    private float turning; // indicará si debemos girar, para hacer acción tambien en FixedUpdate 
     private Rigidbody rb;
-
-
+    WheelController[] wheels; // para ref. WheelColliders a los que aplicamos movimiento (+ propiedades script para tomar decisiones)
+    
 
     // Start is called before the first frame update
     void Start()
     {
-        // --> currentSpeed = new Vector3 (0, 0, 0); ????
-        movement = 0;
-        rb = GetComponent<Rigidbody>();
+        movement = turning = 0;
+        rb = GetComponent<Rigidbody>(); // ??
+        wheels = GetComponentsInChildren<WheelController>();
 
+        // Ajustar centro de masa verticalmente, para evitar que el coche ruede
+        rb.centerOfMass += Vector3.up * centreOfGravityOffset;
     }
 
     // Update is called once per frame
@@ -37,23 +45,48 @@ public class CarController : MonoBehaviour
         else
             movement = 0; // nada
 
-
-        //--> transform.position += currentSpeed * Time.deltaTime;
+        turning = Input.GetAxis("Horizontal");        
     }
 
     // ¡¡Para las físicas!!
     void FixedUpdate(){
 
-        if (movement != 0)
-            Move (movement);
+        float currentSpeed = Vector3.Dot(transform.forward, rb.velocity); // velocidad hacia adelante actual
 
+        // Da cuán de cerca estamos del máximo, como valor entre 0 y 1
+        float speedFactor = Mathf.InverseLerp(0, maxSpeed, currentSpeed);
+
+        // Use that to calculate how much torque is available 
+        // (zero torque at top speed)
+        float currentMotorTorque = Mathf.Lerp(motorTorque, 0, speedFactor);
+
+        // …and to calculate how much to steer 
+        // (the car steers more gently at top speed)
+        float currentTurnRange = Mathf.Lerp(turnAngle, turnAngleAtMaxSpeed, speedFactor);
+
+        foreach (var wheel in wheels){
+            
+            Move (movement, wheel, currentMotorTorque);
+            
+            if (wheel.steerable){
+                if (movement >= 0)
+                    Turn (turning, wheel, currentTurnRange);
+                else
+                    Turn (-turning, wheel, currentTurnRange); // para que al ir para atrás vaya en sentido giro opuesto
+            }
+     
+        }
+   
     }
 
     
-    // Mueve coche adelante o atrás en función del valor value [-1, 1] un máximo de acceleration
-    void Move (float value){
+    // Mueve rueda coche adelante o atrás en función del valor value [-1, 1] un máximo de acceleration y hasta una velocidad MaxSpeed
+    void Move (float value, WheelController wheel, float currentMotorTorque){
 
-        float currentSpeed = Vector3.Dot(transform.forward, rb.velocity);
+        wheel.WheelCollider.motorTorque = value * currentMotorTorque;
+
+
+        /*
         float addedSpeed = acceleration * value;
         float finalSpeed = currentSpeed + addedSpeed;
 
@@ -67,8 +100,21 @@ public class CarController : MonoBehaviour
         else
             rb.AddForce (transform.forward * (MaxSpeed - currentSpeed), ForceMode.Acceleration);
 
+        */
+
         //currentSpeed += transform.forward * acceleration * value * Time.deltaTime;
+        
 
         // OJO --> currentSpeed = Vector3.ClampMagnitude (currentSpeed, MaxSpeed); // capar velocidad máxima, para que no vaya más allá de MaxSpeed
+    }
+
+    // Hace girar rueda del coche en función del valor value [-1, 1] y otros parámetros
+    void Turn (float value, WheelController wheel, float currentTurnRange){
+
+        wheel.WheelCollider.steerAngle = value * currentTurnRange;
+        //float currentSpeed = Vector3.Dot(transform.forward, rb.velocity);
+
+        //rb.AddTorque (Vector3.up * value * currentSpeed * steerAngle); // giramos en función de magnitud de avance y parámetro steerAngle
+
     }
 }
---
+++ Assets/Scripts/WheelController.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WheelController : MonoBehaviour
+{
+    // Propiedades para considerar en el script CarController
+    public bool steerable;
+    public bool motorized;
+
+    [HideInInspector] public WheelCollider WheelCollider; // para facilitar acceso en el CarController
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        WheelCollider = GetComponent<WheelCollider>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
